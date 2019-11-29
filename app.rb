d67a1f0e53da2d8f591098ef1769d6e2f21cd1e5
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-11-29 16:16:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -9,11 +9,18 @@ module FormsLab
     end
 
     get '/new' do
-      
+      erb :'pirates/new'
     end
 
-    post '/show' do
-       
+    post '/pirates' do
+       @pirate=Pirate.new(params[:pirate])
+
+       params[:pirate][:ships].each do |info|
+        Ship.new(info)
+       end
+       @ships=Ship.all
+
+       erb :'pirates/show'
     end
   end
 end
